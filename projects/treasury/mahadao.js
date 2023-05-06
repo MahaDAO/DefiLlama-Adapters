968111f0b1bbe9369920fdb04a9e7f141e1ffa98
Steven Enamakel <enamakel@mahadao.com>
2023-05-06 14:00:01 +0300

    added more assets to track for the MahaDAO treasury

--- projects/treasury/mahadao.js
+++ projects/treasury/mahadao.js
@@ -1,20 +1,25 @@
-const { sumTokensExport, nullAddress } = require("../helper/unwrapLPs.js");
+const { sumTokensExport } = require("../helper/unwrapLPs.js");
 
 const eth = {
+  eth: "0x0000000000000000000000000000000000000000",
   weth: "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2",
   arth: "0x8CC0F052fff7eaD7f2EdCCcaC895502E884a8a71",
   maha: "0x745407c86df8db893011912d3ab28e68b62e49b0",
-  treasury: "0x43c958affe41d44f0a02ae177b591e93c86adbea",
+  dai: "0x6B175474E89094C44Da98b954EedeAC495271d0F",
+  usdc: "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48",
+  usdt: "0xdac17f958d2ee523a2206206994597c13d831ec7",
+  timelock: "0x43c958affe41d44f0a02ae177b591e93c86adbea",
+  ecosystemFund: "0x6357EDbfE5aDA570005ceB8FAd3139eF5A8863CC",
 };
 
 module.exports = {
   ethereum: {
     tvl: sumTokensExport({
-      owner: eth.treasury,
-      tokens: [eth.weth],
+      owners: [eth.timelock, eth.ecosystemFund],
+      tokens: [eth.weth, eth.eth, eth.usdc, eth.usdt, eth.dai],
     }),
     ownTokens: sumTokensExport({
-      owner: eth.treasury,
+      owners: [eth.timelock, eth.ecosystemFund],
       tokens: [eth.arth, eth.maha],
     }),
   },
